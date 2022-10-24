1c522f05cbdbfe43f7da089b7b98b824c3fccd75
George007361 <gera.illarionov@mail.ru>
2022-10-25 00:31:06 +0300

    added app config to docs

--- src/App/Configs/Config.ts
+++ src/App/Configs/Config.ts
@@ -1,3 +1,9 @@
+/**
+ * Основная конфигурация приложения
+ * @category Application
+ * @type {object}
+ * @property {string} APIUrl - URL и порт сервера
+ */
 const config = {
     APIUrl: 'http://127.0.0.1:8080'
 };
