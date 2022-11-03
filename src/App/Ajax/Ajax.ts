f0c7398313b346714297e0643ce3ca74dbc45296
LDK28 <dkostev68@gmail.com>
2022-11-03 12:48:32 +0300

    Change Ajax

---
+++ src/App/Ajax/Ajax.ts
@@ -0,0 +1,39 @@
+import config from "../Configs/Config";
+
+interface IParamsProps {
+    url: string;
+    method: string;
+    headers: {};
+};
+
+/**
+ * Функция асинхронного запроса на сервер
+ * @param  {IParamsProps} params - параметры подключения
+ * @param  {string} body - тело запроса
+ * @return {Promise} - промис запроса 
+ */
+export async function ajax(params: IParamsProps, body?: string) {
+    const response = await fetch(`${config.host}${params.url}`,
+        {
+            method: params.method,
+            headers: params.headers,
+            body: body,
+            credentials: 'include',
+        }
+    );
+
+    let parsedBody;
+    try {
+        parsedBody = await response.json();
+    } catch (error) {
+        console.log(error);
+        parsedBody = {};
+    }
+
+    return {
+        status: response.status,
+        parsedBody
+    };
+}
+
+export default ajax;
