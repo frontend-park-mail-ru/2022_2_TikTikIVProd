7bf2ed56af541bd398b1a1c689e4557c9a061f0e
LDK28 <dkostev68@gmail.com>
2022-11-10 16:02:51 +0300

    Fix csrf

--- src/App/Ajax/Ajax.ts
+++ src/App/Ajax/Ajax.ts
@@ -7,21 +7,6 @@ interface IParamsProps {
 };
 
 
-function getCookie(name: string) {
-    if (!document.cookie) {
-        return null;
-    }
-
-    const xsrfCookies = document.cookie.split(';')
-        .map(c => c.trim())
-        .filter(c => c.startsWith(name + '='));
-
-    if (xsrfCookies.length === 0) {
-        return null;
-    }
-    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
-}
-
 /**
  * Функция асинхронного запроса на сервер
  * @param  {IParamsProps} params - параметры подключения
@@ -32,10 +17,16 @@ export async function ajax(params: IParamsProps, body?: string) {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json;charset=utf-8');
 
-    const csrfToken = getCookie('CSRF-Token');
+    const csrfResponse = await fetch(`${config.host}${config.api.csrf.url}`, {
+        method: config.api.csrf.method,
+        credentials: 'include',
+    });
+
+    const csrfToken = csrfResponse.headers.get('X-CSRF-Token');
     if (csrfToken !== null) {
         headers.append('X-CSRF-Token', csrfToken);
     }
+
 
     let response = await fetch(`${config.host}${params.url}`,
         {
@@ -45,26 +36,6 @@ export async function ajax(params: IParamsProps, body?: string) {
             credentials: 'include',
         }
     );
-
-    if (response.status === 403) {
-        fetch(`${config.host}${config.api.csrf.url}`, {
-            method: config.api.csrf.method
-        })
-
-        const csrfToken = getCookie('CSRF-TOKEN');
-        if (csrfToken !== null) {
-            headers.append('X-CSRFToken', csrfToken);
-        }
-
-        response = await fetch(`${config.host}${params.url}`,
-            {
-                method: params.method,
-                headers: headers,
-                body: body,
-                credentials: 'include',
-            }
-        );
-    }
 
     let parsedBody;
     try {
